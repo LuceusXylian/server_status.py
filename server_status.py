f32a8a85c458dcf37be53979f4f91fc5c0b84603
Xylian <33781686+XylianZeref@users.noreply.github.com>
2020-12-15 14:38:30 +0100

    Update server_status.py

--- server_status.py
+++ server_status.py
@@ -7,26 +7,49 @@
 
 import requests
 import time
+from datetime import datetime
 import urllib3
 from playsound import playsound
 
 urllib3.disable_warnings()
 wavFile_status200 = "servers back online speech.wav"
 wavFile_status500 = "error500.wav"
+wavFile_status408 = "error408.wav"
 wasOffline = False
-wasOnline = False
+wasOnline = True
 
-while True:
-    r = requests.get('https://DOMAIN_ME', verify=False)
-    print(r.status_code)
-    if r.status_code == 500:
+def printTime(text, tag):
+    print(datetime.now(), " [", tag, "] ", text)
+
+
+def test_server_response(url, tag):
+    global wasOffline
+    global wasOnline
+
+    try:
+        r = requests.get(url, verify=False)
+        printTime("status: " + str(r.status_code), tag)
+        if r.status_code == 500:
+            wasOffline = True
+            if wasOnline:
+                playsound(wavFile_status500)
+                wasOnline = False
+        else:
+            wasOnline = True
+            if wasOffline:
+                playsound(wavFile_status200)
+                wasOffline = False
+        pass
+    except requests.RequestException:
+        printTime("timeout", tag)
         wasOffline = True
         if wasOnline:
-            playsound(wavFile_status500)
+            playsound(wavFile_status408)
             wasOnline = False
-    else:
-        wasOnline = True
-        if wasOffline:
-            playsound(wavFile_status200)
-            wasOffline = False
-    time.sleep(4)
+        pass
+
+
+while True:
+    test_server_response('https://DOMAIN_ME', "PRODUCTION")
+    test_server_response('https://DOMAIN_ME', "TESTING")
+    time.sleep(10)
